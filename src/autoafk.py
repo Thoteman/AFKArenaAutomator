dcf4470f1946e87100aab3e971ecedd01bcbe35d
ThomasKnoops <thomas@knoops.be>
2025-06-19 10:20:35 +0200

    fix: bugfixes

--- src/autoafk.py
+++ src/autoafk.py
@@ -8,6 +8,7 @@ import configparser
 DEVICE_ID = ""
 SCRCPY_CLIENT = None
 CONFIG_PATH = "config.ini"
+BACK_BUTTON = (30, 1890)
 
 def connect_emulator(logger):
     global DEVICE_ID
@@ -40,14 +41,12 @@ def stop_scrcpy_client(logger):
     return
 
 def take_screenshot(logger):
-    global DEVICE_ID, SCRCPY_CLIENT
+    global DEVICE_ID, SCRCPY_CLIENT, CONFIG_PATH
     if not DEVICE_ID:
-        logger("No device connected. Please connect to an emulator first.", "error")
-        return
-    
+        connect_emulator(logger)
+
     if not SCRCPY_CLIENT:
-        logger("scrcpy client is not running. Please start it first.", "error")
-        return
+        start_scrcpy_client(logger)
     
     random_text = ''.join(__import__('random').choices(__import__('string').ascii_letters + __import__('string').digits, k=20))
 
@@ -58,6 +57,8 @@ def take_screenshot(logger):
     pil_img.save(f"screenshots/screenshot_{random_text}.png")
 
     logger(f"Screenshot saved to screenshots/screenshot_{random_text}.png", "success")
+
+    stop_scrcpy_client(logger)
 
 def start_daily_tasks(logger):
     global DEVICE_ID, SCRCPY_CLIENT, CONFIG_PATH
@@ -73,16 +74,58 @@ def start_daily_tasks(logger):
     if config['Tasks']['Claim AFK Rewards'] == 'True':
         logger("Starting AFK rewards claim task...", "info")
         claim_afk_rewards(DEVICE_ID, SCRCPY_CLIENT, logger)
-
-    time.sleep(3)
+        time.sleep(3)
 
     if config['Tasks']['Campaign Battle'] == 'True':
         logger("Starting Campaign Battle task...", "info")
         campaign_battle(DEVICE_ID, SCRCPY_CLIENT, logger)
+        time.sleep(3)
 
-    time.sleep(3)
     if config['Tasks']['Claim Fast Rewards'] == 'True':
         logger("Starting Fast Rewards claim task...", "info")
         claim_fast_rewards(DEVICE_ID, SCRCPY_CLIENT, logger, int(config['Tasks']['Amount of Fast Rewards']))
+        time.sleep(3)
+
+    if config['Tasks']['Bounty Board'] == 'True':
+        logger("Starting Bounty Board task...", "info")
+        bounty_board(DEVICE_ID, SCRCPY_CLIENT, logger)
+        time.sleep(3)
+
+    if config['Tasks']['Temporal Rift Fountain'] == 'True':
+        logger("Starting Temporal Rift Fountain task...", "info")
+        temporal_rift(DEVICE_ID, SCRCPY_CLIENT, logger)
+        time.sleep(3)
+
+    if config['Tasks']['King\'s Tower Battle'] == 'True':
+        logger("Starting King's Tower Battle task...", "info")
+        kings_tower(DEVICE_ID, SCRCPY_CLIENT, logger)
+        time.sleep(3)
+
+    if config['Tasks']['Arcane Labyrinth'] == 'True':
+        logger("Starting Arcane Labyrinth task...", "info")
+        arcane_labyrinth(DEVICE_ID, SCRCPY_CLIENT, logger)
+        time.sleep(3)
+
+    if config['Tasks']['Treasure Scramble'] == 'True':
+        logger("Starting Treasure Scramble task...", "info")
+        treasure_scramble(DEVICE_ID, SCRCPY_CLIENT, logger)
+        time.sleep(3)
+
+    if config['Tasks']['Arena of Heroes'] == 'True':
+        logger("Starting Arena of Heroes task...", "info")
+        arena_of_heroes(DEVICE_ID, SCRCPY_CLIENT, logger, int(config['Tasks']['Amount of Arena Battles']))
+        time.sleep(3)
+
+    if config['Tasks']['Legends Challenger Coins'] == 'True':
+        logger("Starting Legends Challenger Coins task...", "info")
+        legends_challenger(DEVICE_ID, SCRCPY_CLIENT, logger)
+        time.sleep(3)
+
+    if config['Tasks']['Legends Championship Betting'] == 'True':
+        logger("Starting Legends Championship Betting task...", "info")
+        legends_championship(DEVICE_ID, SCRCPY_CLIENT, logger)
+        time.sleep(3)
+
+    
 
     stop_scrcpy_client(logger)
--- src/tasks.py
+++ src/tasks.py
@@ -1,6 +1,8 @@
 from adbauto import *
 import time
 
+BACK_BUTTON = (30, 1890)
+
 # All functions related to campaign screen
 def claim_afk_rewards(device_id, scrcpy, logger):
     if find_image(scrcpy.last_frame, "res/campaign/campaign_unselected.png"):
@@ -8,9 +10,9 @@ def claim_afk_rewards(device_id, scrcpy, logger):
         time.sleep(3)
 
     if find_image(scrcpy.last_frame, "res/campaign/campaign_selected.png"):
-        tap(device_id, 540, 1500)
+        tap(device_id,  scrcpy.resolution[0]//2, scrcpy.resolution[1]//2)
         time.sleep(3)
-        tap(device_id, 540, 1500)
+        tap(device_id,  BACK_BUTTON[0], BACK_BUTTON[1])
         if find_image(scrcpy.last_frame, "res/campaign/campaign_selected.png"):
             logger("Claimed AFK rewards!", "success")
 
@@ -34,13 +36,12 @@ def campaign_battle(device_id, scrcpy, logger):
         time.sleep(3)
         tap(device_id, scrcpy.resolution[0] // 2, scrcpy.resolution[1] // 2)
         time.sleep(3)
-        if tap_img_when_visible(device_id, scrcpy, "res/campaign/end_autobattle_button.png", timeout=5, random_delay=True):
+        if tap_img_when_visible(device_id, scrcpy, "res/global/end_autobattle_button.png", timeout=5, random_delay=True):
             success = True
         time.sleep(3)
         tap_img_when_visible(device_id, scrcpy, "res/global/back_arrow.png", timeout=5, random_delay=True)
         logger("Battle started successfully!", "success") if success else logger("Battle failed to start.", "error")
 
-## TODO: fix
 def claim_fast_rewards(device_id, scrcpy, logger, amount):
     if find_image(scrcpy.last_frame, "res/campaign/campaign_unselected.png"):
         tap_image(device_id, scrcpy.last_frame, "res/campaign/campaign_unselected.png")
@@ -48,39 +49,170 @@ def claim_fast_rewards(device_id, scrcpy, logger, amount):
 
     if find_image(scrcpy.last_frame, "res/campaign/campaign_selected.png"):
         if tap_img_when_visible(device_id, scrcpy, "res/campaign/fast_rewards_red.png"):
-            logger("Claimed fast rewards!", "success")
+            time.sleep(3)
+            claim_button = find_image(scrcpy.last_frame, "res/campaign/fast_rewards_free.png")
+            for _ in range(amount):
+                tap(device_id, claim_button[0], claim_button[1])
+                time.sleep(3)
+                tap(device_id, BACK_BUTTON[0], BACK_BUTTON[1])
+                time.sleep(3)
+            tap(device_id, BACK_BUTTON[0], BACK_BUTTON[1])
+            logger(f"Claimed fast rewards {amount} of time(s)!", "success")
             return
         else:
             logger("Fast rewards already claimed.", "info")
 
-# All functions related to dark forest screen
+# All functions related to dark forest screen 
+# TODO: ADD CLAIM FUNCTIONS TO BOUNTY BOARD
 def bounty_board(device_id, scrcpy, logger):
-    pass
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8):
+        tap_image(device_id, scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8)
+        time.sleep(3)
+
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+        tap_image(device_id, scrcpy.last_frame, "res/darkforest/bounty_board.png")
+        time.sleep(3)
+
+        # This is only when an event is active
+        if tap_image(device_id, scrcpy.last_frame, "res/darkforest/event_bounty_unselected.png") or find_image(scrcpy.last_frame, "res/darkforest/event_bounty_selected.png"):
+            time.sleep(3)
+            while tap_image(device_id, scrcpy.last_frame, "res/darkforest/event_bounty_dispatch.png"):
+                time.sleep(3)
+                tap_img_when_visible(device_id, scrcpy, "res/darkforest/event_bounty_herochoice.png", timeout=5, random_delay=True, threshold=0.8)
+                time.sleep(3)
+                tap(device_id, 118, 1482) # This is the upper left hero (what the game advices to send)
+                time.sleep(3)
+                tap_img_when_visible(device_id, scrcpy, "res/darkforest/event_bounty_start.png", timeout=5, random_delay=True)
+                time.sleep(3)
+
+        # This is the solo bounty board TODO: Make it possible to send each bounty on their own
+        if tap_image(device_id, scrcpy.last_frame, "res/darkforest/solo_bounty_unselected.png") or find_image(scrcpy.last_frame, "res/darkforest/solo_bounty_selected.png"):
+            time.sleep(3)
+            if tap_img_when_visible(device_id, scrcpy, "res/darkforest/solo_bounty_dispatch.png", timeout=5, random_delay=True):
+                time.sleep(3)
+                tap_img_when_visible(device_id, scrcpy, "res/darkforest/solo_bounty_start.png")
+                time.sleep(3)
+
+        # This is the team bounty board
+        if tap_image(device_id, scrcpy.last_frame, "res/darkforest/team_bounty_unselected.png") or find_image(scrcpy.last_frame, "res/darkforest/team_bounty_selected.png"):
+            time.sleep(3)
+            if tap_img_when_visible(device_id, scrcpy, "res/darkforest/solo_bounty_dispatch.png", timeout=5, random_delay=True):
+                time.sleep(3)
+                tap_img_when_visible(device_id, scrcpy, "res/darkforest/solo_bounty_start.png")
+                time.sleep(3)
+
+        tap_img_when_visible(device_id, scrcpy, "res/global/back_arrow.png", timeout=5, random_delay=True)
+        logger("Bounty Board tasks completed!", "success")
+            
+    else:
+        logger("Bounty Board start button not found.", "error")
 
 def temporal_rift(device_id, scrcpy, logger):
-    pass
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8):
+        tap_image(device_id, scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8)
+        time.sleep(3)
+
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/temporal_rift.png", threshold=0.8)
+        time.sleep(3)
+        while not find_image(scrcpy.last_frame, "res/darkforest/temporal_fountain.png", threshold=0.8):
+            tap(device_id, BACK_BUTTON[0], BACK_BUTTON[1])
+            time.sleep(3)
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/temporal_fountain.png", timeout=5, random_delay=True)
+        time.sleep(3)
+        while not find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+            tap(device_id, BACK_BUTTON[0], BACK_BUTTON[1])
+            time.sleep(3)
+        logger("Temporal Rift tasks completed!", "success")
 
 def kings_tower(device_id, scrcpy, logger):
-    pass
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8):
+        tap_image(device_id, scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8)
+        time.sleep(3)
+
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/kings_tower.png", threshold=0.8)
+        time.sleep(3)
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/kings_tower_main.png", timeout=5, random_delay=True)
+        time.sleep(3)
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/kings_tower_battle.png", timeout=5, random_delay=True)
+        time.sleep(3)
+        tap_img_when_visible(device_id, scrcpy, "res/global/begin_autobattle_button.png", timeout=5, random_delay=True)
+        time.sleep(3)
+        tap_img_when_visible(device_id, scrcpy, "res/global/confirm_begin_autobattle_button.png", timeout=5, random_delay=True)
+        time.sleep(3)
+        tap(device_id, scrcpy.resolution[0] // 2, scrcpy.resolution[1] // 2)
+        time.sleep(3)
+        if tap_img_when_visible(device_id, scrcpy, "res/global/end_autobattle_button.png", timeout=5, random_delay=True):
+            success = True
+        else:
+            success = False
+        time.sleep(3)
+        while not find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+            tap(device_id, BACK_BUTTON[0], BACK_BUTTON[1])
+            time.sleep(3)
+        logger("King's tower started successfully!", "success") if success else logger("King's tower failed to start.", "error")
+
+    
 
 def arcane_labyrinth(device_id, scrcpy, logger):
-    pass
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8):
+        tap_image(device_id, scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8)
+        time.sleep(3)
+
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/arcane_labyrinth.png", threshold=0.8)
+        time.sleep(3)
+        #TODO: Implement Arcane Labyrinth tasks
 
 # All functions related to Arena of Heroes screen
-def treasure_vanguard(device_id, scrcpy, logger):
-    pass
-
 def treasure_scramble(device_id, scrcpy, logger):
-    pass
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8):
+        tap_image(device_id, scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8)
+        time.sleep(3)
 
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/arena.png", threshold=0.8)
+        time.sleep(3)
+        # TODO: change numbers when new seasons start and I get screenshots
+        if tap_image(device_id, scrcpy.last_frame, "res/darkforest/treasure_scramble_1.png", timeout=5, random_delay=True) or \
+          tap_image(device_id, scrcpy.last_frame, "res/darkforest/treasure_scramble_1.png", timeout=5, random_delay=True) or \
+          tap_image(device_id, scrcpy.last_frame, "res/darkforest/treasure_scramble_1.png", timeout=5, random_delay=True) or \
+          tap_image(device_id, scrcpy.last_frame, "res/darkforest/treasure_scramble_1.png", timeout=5, random_delay=True):
+            time.sleep(3)
+            
+# TODO
 def arena_of_heroes(device_id, scrcpy, logger):
-    pass
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8):
+        tap_image(device_id, scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8)
+        time.sleep(3)
+
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/arena.png", threshold=0.8)
+        time.sleep(3)
+
 
 def legends_challenger(device_id, scrcpy, logger):
-    pass
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8):
+        tap_image(device_id, scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8)
+        time.sleep(3)
 
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/arena.png", threshold=0.8)
+        time.sleep(3)
+        scroll(device_id, "up", 500, 500, scrcpy.resolution[0] // 2, scrcpy.resolution[1] // 2)
+        if tap_img_when_visible(device_id, scrcpy, "res/darkforest/legends_challenger.png", threshold=0.8):
+            time.sleep(3)
+
+# TODO
 def legends_championship(device_id, scrcpy, logger):
-    pass
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8):
+        tap_image(device_id, scrcpy.last_frame, "res/darkforest/darkforest_unselected.png", threshold=0.8)
+        time.sleep(3)
+
+    if find_image(scrcpy.last_frame, "res/darkforest/darkforest_selected.png", threshold=0.8):
+        tap_img_when_visible(device_id, scrcpy, "res/darkforest/arena.png", threshold=0.8)
+        time.sleep(3)
 
 # All functions related to Ranhorn screen
 def store(device_id, scrcpy, logger):
